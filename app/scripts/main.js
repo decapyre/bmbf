8f30d99d77615ef6a96f729433fa3fac7fd5f323
Bryan Berger <bryan@bryanberger.com>
2014-10-25 03:35:02 -0400

    Auto scroll to top to prevent headaches with login window on Mobile (scrolling vertically with position: fixed is impossible) Created a header.scss file to extrapolate that stuff. Started inlining media queries for ease of use. Limiting box-shadow blurs for small-only hopefully to render faster. May have todo more in this regard.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -60,7 +60,7 @@ $(function(){
 			back_text: 'Back', // Define what you want your custom back text to be if custom_back_text: true
 			is_hover: false,
 			mobile_show_parent_link: false, // will copy parent links into dropdowns for mobile navigation
-			scrolltop: false // jump to top when sticky nav menu toggle is clicked
+			scrolltop: true // jump to top when sticky nav menu toggle is clicked
 		}
 	});
 
