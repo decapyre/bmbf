882f1e59c7528bc8e5b295efcc3df42316466fac
Bryan Berger <bryan@bryanberger.com>
2014-12-11 17:42:05 -0500

    f-dropdown styles more-services styles updated print styles font half size movie dropdown preferred

--- app/static-assets/js/BMBF.movie.js
+++ app/static-assets/js/BMBF.movie.js
@@ -6,9 +6,11 @@
 	var gwiid = movie.data('gwi-id');
 	var gowatchit = $('.gowatchit');
 	var movieTitle = $('.title').text();//$('meta[property="og\\:title"]').attr('content');
-	var markup = '';
 	var amazonId = 'bemobyfa09-20';
-	var usedServiceNames = [];
+	var services = {};
+	var serviceNames = [];
+	var preferredServices = ['amazon', 'googleplay', 'netflix', 'hulu', 'redbox', 'itunes', 'youtube'];
+	var numServicesToShow = 3;
 
 	_private = {
 		bind: function() {
@@ -38,7 +40,7 @@
 						if(typeof data.movie.onlines !== 'undefined') {
 							if(data.movie.onlines !== null) {
 								$.each(data.movie.onlines, function(key, value) {
-									_private.addGoWatchItItem(value);
+									_private.addService(value);
 								});
 							}
 						}
@@ -46,14 +48,14 @@
 						if(typeof data.movie.dvds !== 'undefined') {
 							if(data.movie.dvds !== null) {
 								$.each(data.movie.dvds, function(key, value) {
-									_private.addGoWatchItItem(value);
+									_private.addService(value);
 								});
 							}
 						}
 
-						if(markup !== '') {
+						if(!$.isEmptyObject(services)) {
 							// render list
-							gowatchit.html(markup);
+							_private.renderMarkup();
 						} else {
 							// no online or dvd versions available
 							_private.goWatchItNotFound();
@@ -72,12 +74,49 @@
 			}
 		},
 
-		addGoWatchItItem: function(value) {
-			var serviceName = value.name.toLowerCase().replace(/\s+/g, '');
+		renderMarkup: function() {
+			var j = 0;
+			var markup = '';
+			var moreMarkup = '<li><a class="more-services-trigger" data-dropdown="more-services" aria-controls="more-services" aria-expanded="false">More <i class="bmbf bmbf-caret-down"></i></a>';
+			moreMarkup += '<ul id="more-services" data-dropdown-content class="f-dropdown content gowatchit-dd" aria-hidden="true" tabindex="-1">';
 
-			if(usedServiceNames.length >= 0 && $.inArray(serviceName, usedServiceNames) === -1) {
-				markup += '<li><a href="'+value.watch_now+'" title="'+value.name+'" class="gw-service" data-service="'+serviceName+'"><i class="bmbf bmbf-gw-'+serviceName+'"></i></a></li>';
-				usedServiceNames.push(serviceName);
+			// sort by preferred services first
+			serviceNames.sort(function(a, b) {
+				var x = $.inArray(b, preferredServices), y = $.inArray(a, preferredServices);
+				return x < y ? -1 : x > y ? 1 : 0;
+			});
+
+			// build markup
+			for(var i=0; i<serviceNames.length; i++) {
+				var serviceName = serviceNames[i];
+				if(services.hasOwnProperty(serviceName) ) {
+					var service = services[serviceName];
+					if(j < numServicesToShow) {
+						markup += '<li><a href="'+service.watch_now+'" title="'+service.name+'" class="gw-service" data-service="'+serviceName+'"><i class="bmbf bmbf-gw-'+serviceName+' bmbf-4x-half"></i></a></li>';
+						j++;
+					} else {
+						moreMarkup += '<li><a href="'+service.watch_now+'" title="'+service.name+'" class="gw-service" data-service="'+serviceName+'"><i class="bmbf bmbf-gw-'+serviceName+' bmbf-4x-half"></i></a></li>';
+					}
+				}
+			}
+
+			// render it
+			gowatchit.html(markup);
+
+			// if we have to show moreMarkup render it
+			if(serviceNames.length > numServicesToShow) {
+				moreMarkup += '</ul></li>';
+				gowatchit.append(moreMarkup);
+				moreMarkup = null;
+			}
+		},
+
+		addService: function(service) {
+			var serviceName = service.name.toLowerCase().replace(/\s+/g, '');
+
+			if(services[serviceName] === undefined) {
+				services[serviceName] = service;
+				serviceNames.push(serviceName);
 			}
 		},
 
@@ -85,13 +124,10 @@
 			BMBF.libs.tracking.track('link', 'click', 'NO GoWatchIt- ' + window.location);
 
 			// at the very least provide a link to an Amazon search result if nothing is found(yes even on amazon from GW)
-			var render = (markup === '') ? true : false;
 			var amazonURI = 'http://www.amazon.com/gp/search?ie=UTF8&camp=1789&creative=9325&index=dvd&keywords='+encodeURIComponent(movieTitle)+'&linkCode=ur2&tag='+amazonId;//+'&linkId=OPSO2DBSGMFPHDYH';
-			markup += '<li><a href="'+amazonURI+'" title="'+movieTitle+'"><i class="bmbf bmbf-6x bmbf-gw-amazon"></i></a></li>';
+			var markup = '<li><a href="'+amazonURI+'" title="'+movieTitle+'"><i class="bmbf bmbf-5x bmbf-gw-amazon"></i></a></li>';
 
-			if(render) {
-				gowatchit.html(markup);
-			}
+			gowatchit.html(markup);
 		},
 
 		setupLoggedInDOM: function() {
